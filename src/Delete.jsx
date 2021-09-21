850f741329187c15023cdb7d23da22a3397ba372
vishal-1codes <pawarvishal10k@gmail.com>
2021-09-21 22:54:06 +0530

    Adding all files to factsbook git hub repo

--- src/Delete.jsx
+++ src/Delete.jsx
@@ -5,7 +5,7 @@ import logo from './favicon.ico';
 
  const Delete=()=>{
   
-  const url="http://172.31.6.14:5000/deletefacts"
+  const url="http://3.108.250.19/deletefacts"
   const [data,setData]=useState({
    id:"", 
   })
